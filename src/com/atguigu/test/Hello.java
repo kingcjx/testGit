c83e642a3210e80bf0afad7df8f757f4c4750e82
kingcjx <2312491020@qq.com>
2016-12-06 09:48:11 +0800

    123

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("first commit");
 		System.out.println("first commit");
+		System.out.println("second commit");
 	}
 }
