a1621c4b8b7a476a8272d286e9249f7e728c2969
kingcjx <2312491020@qq.com>
2016-12-06 18:00:42 +0800

    333

--- src/com/atguigu/test/Hello.java
+++ src/com/atguigu/test/Hello.java
@@ -1,11 +1,12 @@
 package com.atguigu.test;
 
 public class Hello {
-	
+
 	public static void main(String[] args) {
 		System.out.println("first commit");
 		System.out.println("first commit");
 		System.out.println("second commit");
-System.out.println("second commit");
+		System.out.println("second commit");
+		System.out.println("third  commit");
 	}
 }
